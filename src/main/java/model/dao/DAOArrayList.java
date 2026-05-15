d34ce10634537508eb651d999161aeaaa7abbfb1
Roger Serrats <rogerservic@gmail.com>
2026-05-15 13:18:10 +0200

    Commit prueba Roger

--- src/main/java/model/dao/DAOArrayList.java
+++ src/main/java/model/dao/DAOArrayList.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * @version 1.1.0
  */
 public class DAOArrayList implements IDAO{
-    //Para probar commit
+    //Para probar commit de Roger
     ArrayList <Person> people = new ArrayList<>();
 
     @Override
