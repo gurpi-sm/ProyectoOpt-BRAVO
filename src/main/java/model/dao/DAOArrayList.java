c77f2cb8d9a3bbad8460f8efa7aa09e11d4dcd23
CapiGurpi <gurpicapitan@gmail.com>
2026-05-15 13:16:40 +0200

    Commit prueba

--- src/main/java/model/dao/DAOArrayList.java
+++ src/main/java/model/dao/DAOArrayList.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * @version 1.1.0
  */
 public class DAOArrayList implements IDAO{
-    
+    //Para probar commit
     ArrayList <Person> people = new ArrayList<>();
 
     @Override
